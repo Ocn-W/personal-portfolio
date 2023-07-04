6631311f314b0962945b433d4b36bffbb79fcc66
oc34n <ocean.wrng@gmail.com>
2023-07-03 22:51:33 -0400

    Added Netlify CLI & About Me Section
    >Styled About Me section with infinite slideshow element
    >Working on Projects section layout
    >Working on Hobbies section layout

--- src/component/HomePage.jsx
+++ src/component/HomePage.jsx
@@ -1,35 +1,49 @@
 import React, { useRef } from 'react';
 import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 import Lottie from 'lottie-react';
-import animationData from '../assets/svg/101786-wave.json'; 
+import waveSVG from '../assets/svg/101786-wave.json';
+import artSVG from '../assets/svg/70021-abstract-painting-loader.json';
 import '../css/HomePage.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
 import { faLinkedin, faGithub, faHtml5, faCss3, faJsSquare, faReact } from '@fortawesome/free-brands-svg-icons';
+import Projects from './Projects';
 
 export default function HomePage() {
   const ref = useRef();
 
   return (
     <div className="homePage">
-      <nav>
-        <h1 ref={ref} onClick={() => ref.current.scrollTo(0)}className="logo">
-          OCEAN
-          <span
-            style={{
-              color: "rgb(217, 4, 41)",
-              fontFamily: "Helvetica",
-              fontSize: "65px",
-              margin: "0",
-            }}>.</span>
-        </h1>
-        <ul className="navOptions">
-          <li ref={ref} onClick={() => ref.current.scrollTo(2)}>PROJECTS</li>
-          <li>ARTWORK</li>
-          <li>CONTACT</li>
-        </ul>
-      </nav>
-      <Parallax pages={3} ref={ref}>
+      <Parallax pages={5} ref={ref}>
+        <ParallaxLayer sticky={{start: 0, end: 2}} style={{height:'fit-content'}}>
+          <nav>
+            <h1
+              ref={ref}
+              onClick={() => ref.current.scrollTo(0)}
+              className="logo"
+            >
+              OCEAN
+              <span
+                style={{
+                  color: "rgb(217, 4, 41)",
+                  fontFamily: "Helvetica",
+                  fontSize: "65px",
+                  margin: "0",
+                }}
+              >
+                .
+              </span>
+            </h1>
+            <ul className="navOptions">
+              <li ref={ref} onClick={() => ref.current.scrollTo(2)}>
+                PROJECTS
+              </li>
+              <li>ARTWORK</li>
+              <li>CONTACT</li>
+            </ul>
+          </nav>
+        </ParallaxLayer>
+
         <ParallaxLayer speed={1}>
           <section className="introSection">
             <div style={{ display: "flex" }}>
@@ -40,7 +54,8 @@ export default function HomePage() {
                   Based in NYC{"  "}
                   <FontAwesomeIcon
                     icon={faLocationDot}
-                    style={{ color: "red" }}/>
+                    style={{ color: "red" }}
+                  />
                 </p>
                 <div className="contactBtns">
                   <a
@@ -54,65 +69,93 @@ export default function HomePage() {
                   </a>
                 </div>
               </div>
-              <div className="profileImg">
-                
-              </div>
-            </div>
-          </section>
-        </ParallaxLayer>
-        <ParallaxLayer speed={2} offset={0.7} factor={0.5}>
-           <Lottie className="lottie" animationData={animationData} /> 
-        </ParallaxLayer>
-        <ParallaxLayer offset={1} speed={0.1}>
-          <section className="aboutMe">
-            <h2>About Me</h2>
-            <p>
-              Hello! I am a junior React web developer based in NYC. Currently
-              building a strong and growing skill set in JavaScript,
-              React, Git, and automated testing using the Mocha
-              framework in Node or Jest. With a solid foundation in HTML and
-              CSS, I have a keen eye for design and a commitment to producing
-              high-quality, accessible, and responsive code. I am dedicated to
-              continuous learning and staying up-to-date with the latest trends
-              and best coding practices, ensuring that I can provide efficient
-              solutions. Let's connect and discuss how I can
-              contribute to your web development team!
-            </p>
-          </section>
-          <section className="techStack">
-            <h2>MY TECHSTACK</h2>
-            <div className="stackLogos">
-              <FontAwesomeIcon
-                icon={faHtml5}
-                style={{ height: "50px", color: "orangered" }}
-              />
-              <FontAwesomeIcon
-                icon={faCss3}
-                style={{ height: "50px", color: "blue" }}
-              />
-              <FontAwesomeIcon
-                icon={faJsSquare}
-                style={{ height: "50px", color: "gold" }}
-              />
-              <FontAwesomeIcon
-                icon={faReact}
-                style={{ height: "50px", color: "cyan" }}
-              />
-              <img
-                src="src/assets/svg/Vitejs-logo.svg"
-                style={{ height: "50px" }}
-              />
-              <img
-                src="src/assets/svg/Netlify_logo.svg"
-                style={{ height: "50px" }}
-              />
+              <div className="profileImg"></div>
             </div>
           </section>
         </ParallaxLayer>
 
-        <ParallaxLayer offset={2} speed={1}>
+        <ParallaxLayer speed={2} offset={0.7} factor={0.5}>
+          <Lottie className="lottie" animationData={waveSVG} />
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={1} speed={0.1}>
+          <section className="aboutMe">
+            <div className="aboutIntro">
+              <h2>ABOUT ME</h2>
+              <p>
+                Hello! I am a junior React web developer based in NYC. Currently
+                building a strong and growing skill set in JavaScript, React,
+                Git, and automated testing using the Mocha framework in Node or
+                Jest. With a solid foundation in HTML and CSS, I have a keen eye
+                for design and a commitment to producing high-quality,
+                accessible, and responsive code. I am dedicated to continuous
+                learning and staying up-to-date with the latest trends and best
+                coding practices, ensuring that I can provide efficient
+                solutions. Let's connect and discuss how I can contribute to
+                your web development team!
+              </p>
+            </div>
+          </section>
+          <section className="techStack">
+            <h2>MY TECHSTACK</h2>
+            <div className='scrollStack'>
+            <div className="stackLogos">
+              <FontAwesomeIcon icon={faHtml5} style={{ height: "50px", color: "orangered" }}/>
+              <FontAwesomeIcon icon={faCss3} style={{ height: "50px", color: "blue" }}/>
+              <FontAwesomeIcon icon={faJsSquare} style={{ height: "50px", color: "gold" }}/>
+              <FontAwesomeIcon icon={faReact} style={{ height: "50px", color: "cyan" }}/>
+              <img src="src/assets/svg/Vitejs-logo.svg" style={{ height: "50px" }}/>
+              <img src="src/assets/svg/Netlify_logo.svg" style={{ height: "50px" }}/>
+            </div>
+            <div className="stackLogos" aria-hidden="true">
+              <FontAwesomeIcon icon={faHtml5} style={{ height: "50px", color: "orangered" }}/>
+              <FontAwesomeIcon icon={faCss3} style={{ height: "50px", color: "blue" }}/>
+              <FontAwesomeIcon icon={faJsSquare} style={{ height: "50px", color: "gold" }}/>
+              <FontAwesomeIcon icon={faReact} style={{ height: "50px", color: "cyan" }}/>
+              <img src="src/assets/svg/Vitejs-logo.svg" style={{ height: "50px" }}/>
+              <img src="src/assets/svg/Netlify_logo.svg" style={{ height: "50px" }}/>
+            </div>
+            </div>
+          </section>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={2} style={{display:'flex', flexDirection:'column'}}>
           <section className="projectSection">
             <h2>PROJECTS</h2>
+            <Projects />
+          </section>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={4} speed={1}>
+          <section className="hobbySection">
+            <div className="aboutHobbies">
+              <h3>MY HOBBIES</h3>
+              <div>
+                <p>
+                  painting, museums, galleries, anything art related I'm all for
+                  it!
+                </p>
+                <Lottie animationData={artSVG} />
+              </div>
+              <div>
+                <p>
+                  nature, a huge outdoor person in general! skateboarding,
+                  surfing, snowboarding, hiking you name it!
+                </p>
+              </div>
+              <div>
+                <p>
+                  movies! i am a huge fan of horror movies and anything
+                  psychological
+                </p>
+              </div>
+              <div>
+                <p>
+                  fashion! coming from six years in the modeling industry i have
+                  a huge respect for the fahsion industry and clothing!
+                </p>
+              </div>
+            </div>
           </section>
         </ParallaxLayer>
       </Parallax>
--- src/component/Projects.jsx
+++ src/component/Projects.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
 import '../css/Projects.css';
+import retailWebsite from '../assets/RetailSiteHome.jpg';
+import spotifyAPISite from '../assets/soundStackDemo.jpg';
+import theDesktopSite from '../assets/theDesktop02.png';
 
 export default function Projects() {
   return (
     <>
+      <section className='project'>
+      <div className='projectImg1'></div>
+        <div className='projectText'>
+        <p>Project Name</p>
+        <p>Languages Used:</p>
+        <p>Project Info</p>
+        <a>Live Demo</a>
+        </div>
+      </section>
 
+      <section className='project'>
+        <div className='projectImg2'></div>
+        <div className='projectText'>
+        <p>Project Name</p>
+        <p>Languages Used:</p>
+        <p>Project Info</p>
+        <a>Live Demo</a>
+        </div>
+      </section>
+
+      <section className='project'>
+      <div className='projectImg3'></div>
+        <div className='projectText'>
+        <p>Project Name</p>
+        <p>Languages Used:</p>
+        <p>Project Info</p>
+        <a>Live Demo</a>
+        </div>
+      </section>
     </>
   )
 }
